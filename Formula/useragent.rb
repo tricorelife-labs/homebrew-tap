f9a6762a5479b3d3ee344035e9ce61730cfab6d5
TriCoreLife <tricorelife@gmail.com>
2026-04-07 20:48:50 +0800

    fix: point v0.2.3 formula to public useragent-releases repo

--- Formula/useragent.rb
+++ Formula/useragent.rb
@@ -5,7 +5,7 @@ class Useragent < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/tricorelife-labs/UserAgent/releases/download/v0.2.3/UserAgent-aarch64-apple-darwin.tar.xz"
+      url "https://github.com/tricorelife-labs/useragent-releases/releases/download/v0.2.3/UserAgent-aarch64-apple-darwin.tar.xz"
       sha256 "a2e9ae6880b334d8cc8a27a4828aac7cff145c7e3705c09e5e9a6cea3e00bb7c"
 
       def install
@@ -13,7 +13,7 @@ class Useragent < Formula
       end
     end
     on_intel do
-      url "https://github.com/tricorelife-labs/UserAgent/releases/download/v0.2.3/UserAgent-x86_64-apple-darwin.tar.xz"
+      url "https://github.com/tricorelife-labs/useragent-releases/releases/download/v0.2.3/UserAgent-x86_64-apple-darwin.tar.xz"
       sha256 "8751b91a15a7c9fd8f83e68c864dc64abb3149fa4c7323b8c2908c9c19c7749e"
 
       def install
