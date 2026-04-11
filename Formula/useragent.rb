b82c3d88fbef57cb06b6308da7d9b5b552d914ed
TriCoreLife <tricorelife@gmail.com>
2026-04-11 23:39:54 +0800

    useragent v0.2.6

--- Formula/useragent.rb
+++ Formula/useragent.rb
@@ -1,20 +1,20 @@
 class Useragent < Formula
   desc "A powerful AI agent CLI with multi-model support and tool use"
   homepage "https://github.com/tricorelife-labs/useragent-releases"
-  version "0.2.5"
+  version "0.2.6"
 
   on_macos do
     on_arm do
-      url "https://github.com/tricorelife-labs/useragent-releases/releases/download/v0.2.5/UserAgent-aarch64-apple-darwin.tar.xz"
-      sha256 "64703a43fadced2c20e1ca3481ce22f3e187428323292186d2acf0ac9429bf77"
+      url "https://github.com/tricorelife-labs/useragent-releases/releases/download/v0.2.6/UserAgent-aarch64-apple-darwin.tar.xz"
+      sha256 "ad4b091b76e720e164b00a2a8f1b786614fcb3141365dd730bc6a4e655869e1e"
 
       def install
         bin.install "useragent-cli" => "useragent"
       end
     end
     on_intel do
-      url "https://github.com/tricorelife-labs/useragent-releases/releases/download/v0.2.5/UserAgent-x86_64-apple-darwin.tar.xz"
-      sha256 "d548a6a660b82591be444018b1bb52c0b345c488c530287c2659f923c977099a"
+      url "https://github.com/tricorelife-labs/useragent-releases/releases/download/v0.2.6/UserAgent-x86_64-apple-darwin.tar.xz"
+      sha256 "7d7f0641a57f87bb59e43b7894c6c722dbefc902b02fee747232c9bedabd9e62"
 
       def install
         bin.install "useragent-cli" => "useragent"
