3fb7831d7d2876bfd6132b1e76e49906a444ecc4
TriCoreLife <tricorelife@gmail.com>
2026-05-03 07:43:23 +0000

    useragent v0.3.24

--- Formula/useragent.rb
+++ Formula/useragent.rb
@@ -1,66 +1,28 @@
 class Useragent < Formula
   desc "A powerful AI agent CLI with multi-model support and tool use"
-  homepage "https://github.com/tricorelife-labs/UserAgent"
+  homepage "https://github.com/tricorelife-labs/useragent-releases"
   version "0.3.24"
-  if OS.mac?
-    if Hardware::CPU.arm?
-      url "https://github.com/tricorelife-labs/UserAgent/releases/download/v0.3.24/UserAgent-aarch64-apple-darwin.tar.xz"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/tricorelife-labs/useragent-releases/releases/download/v0.3.24/UserAgent-aarch64-apple-darwin.tar.xz"
       sha256 "16486b06dc01688094b56b51e95b08cf0eefc7d8f4c935ebf6fa605d8a73fbd2"
+
+      def install
+        bin.install "useragent-cli" => "useragent"
+      end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/tricorelife-labs/UserAgent/releases/download/v0.3.24/UserAgent-x86_64-apple-darwin.tar.xz"
+    on_intel do
+      url "https://github.com/tricorelife-labs/useragent-releases/releases/download/v0.3.24/UserAgent-x86_64-apple-darwin.tar.xz"
       sha256 "e626ebda2d807e31fbfca7fddd78e079297f9420c379ad8c5c3ba4be5cc53797"
-    end
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tricorelife-labs/UserAgent/releases/download/v0.3.24/UserAgent-x86_64-unknown-linux-gnu.tar.xz"
-    sha256 "5a18cc0ce7942eea8e2969dbdc90c20e630a871ebe9ee2bd548e0b554d31b2b0"
-  end
-  license "MIT"
 
-  BINARY_ALIASES = {
-    "aarch64-apple-darwin":     {},
-    "x86_64-apple-darwin":      {},
-    "x86_64-unknown-linux-gnu": {},
-  }.freeze
-
-  def target_triple
-    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
-    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
-
-    "#{cpu}-#{os}"
-  end
-
-  def install_binary_aliases!
-    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
-      dests.each do |dest|
-        bin.install_symlink bin/source.to_s => dest
+      def install
+        bin.install "useragent-cli" => "useragent"
       end
     end
   end
 
-  def install
-    if OS.mac? && Hardware::CPU.arm?
-      bin.install "axum_server", "cli", "comic_video_generator", "save_image_from_response", "segmented_video_gen",
-"server", "test_proxy", "tricore_agent", "useragent-cli"
-    end
-    if OS.mac? && Hardware::CPU.intel?
-      bin.install "axum_server", "cli", "comic_video_generator", "save_image_from_response", "segmented_video_gen",
-"server", "test_proxy", "tricore_agent", "useragent-cli"
-    end
-    if OS.linux? && Hardware::CPU.intel?
-      bin.install "axum_server", "cli", "comic_video_generator", "save_image_from_response", "segmented_video_gen",
-"server", "test_proxy", "tricore_agent", "useragent-cli"
-    end
-
-    install_binary_aliases!
-
-    # Homebrew will automatically install these, so we don't need to do that
-    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
-    leftover_contents = Dir["*"] - doc_files
-
-    # Install any leftover files in pkgshare; these are probably config or
-    # sample files.
-    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
+  test do
+    assert_match version.to_s, shell_output("#{bin}/useragent --version 2>&1")
   end
 end
